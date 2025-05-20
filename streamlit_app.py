90a0516ea5a6bfe8e30272f73014a5e79d607bd2
Muhammad Umair Akram <164014544+imumairakram@users.noreply.github.com>
2025-05-20 17:59:02 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,35 +10,36 @@ st.write("Choose the fruits you want in your custom Smoothie!")
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
+try:
+    cnx = st.connection("Snowflake")
+    session = cnx.session()
+    
+    my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+    
+    ingredients_List = st.multiselect(
+        'Choose up to 5 ingredients:',
+        my_dataframe,
+        max_selections=5
+    )
+    
+    if ingredients_List:
+        ingredients_string = ' '.join(ingredients_List)
+    
+        my_insert_stmt = f"""
+            INSERT INTO smoothies.public.orders(ingredients, name_on_order)
+            VALUES ('{ingredients_string.strip()}', '{name_on_order}')
+        """
+    
+        st.write(my_insert_stmt)
+    
+        time_to_insert = st.button('Submit Order')
+    
+        if time_to_insert:
+            session.sql(my_insert_stmt).collect()
+            st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
-cnx = st.connection("Snowflake")
-session = cnx.session()
-
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-
-ingredients_List = st.multiselect(
-    'Choose up to 5 ingredients:',
-    my_dataframe,
-    max_selections=5
-)
-
-if ingredients_List:
-    ingredients_string = ' '.join(ingredients_List)
-
-    my_insert_stmt = f"""
-        INSERT INTO smoothies.public.orders(ingredients, name_on_order)
-        VALUES ('{ingredients_string.strip()}', '{name_on_order}')
-    """
-
-    st.write(my_insert_stmt)
-
-    time_to_insert = st.button('Submit Order')
-
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
-
-
+except Exception as e:
+    st.error(f"An error occurred: {e}")
 
 # # Import python packages
 # import streamlit as st
