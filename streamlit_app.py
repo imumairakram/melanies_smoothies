01163b9e4aede47c44fc5bf87f158ff3fad544fa
Muhammad Umair Akram <164014544+imumairakram@users.noreply.github.com>
2025-05-20 17:27:47 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@
 # from snowflake.snowpark.functions import col
 
 # # Write directly to the app
-# st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+# st.title("My Parent's New Healthy Dinner")
 # st.write(
 #   """Choose the fruits you want in your custom Smoothie!
 #   """
