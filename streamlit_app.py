c1356278c57197aad5f4560475ce82ceb16e34c4
Muhammad Umair Akram <164014544+imumairakram@users.noreply.github.com>
2025-05-21 13:31:58 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import requests
 
 # Write directly to the app
-st.title("My Parent's New Healthy Dinner")
+st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
 
 name_on_order = st.text_input('Name on Smoothie:')
