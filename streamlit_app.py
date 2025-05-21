7ef89e1aa8df760b8be0fc782a40bd0fbca32f06
Muhammad Umair Akram <164014544+imumairakram@users.noreply.github.com>
2025-05-21 20:35:53 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,10 +30,10 @@ try:
         ingredients_string = ' '
 
         for fruit_chosen in ingredients_List:
-        ingredients_string += fruit_chosen + ' '
-        st.subheader(f"{fruit_chosen} Nutrition Information")
-        # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-        # sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+            ingredients_string += fruit_chosen + ' '
+            st.subheader(f"{fruit_chosen} Nutrition Information")
+            # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+            # sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
         # SQL Insert Statement
         my_insert_stmt = f"""
