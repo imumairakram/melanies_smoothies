fb51e7d0fb59ee124b7c9e065883e2707b29bd85
Muhammad Umair Akram <164014544+imumairakram@users.noreply.github.com>
2025-05-21 23:30:16 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,8 +91,8 @@
 
      # Query fruit options
      my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON')
-     st.dataFrame(data = my_dataframe, use_container_width=True)
-     st.stop()
+     # st.dataFrame(data = my_dataframe, use_container_width=True)
+     # st.stop()
 
      # Multiselect for ingredients
      ingredients_list = st.multiselect(
@@ -111,7 +111,11 @@
             st.subheader(f"{fruit} Nutrition Info")
 
             search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-            st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+            # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+            st.subheader(fruit_chosen + Nutrition Information') 
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on) 
+            fv_df st.dataframe(data=fruityvice_response.json(), use_container_width=True) 
+                    
             
             response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{api_fruit.lower()}")
             if response.status_code == 200:
